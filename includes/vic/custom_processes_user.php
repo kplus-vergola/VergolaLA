8a468a3298d5064132fabc5932a16a17d07915c2
Seng.Leong <Seng.Leong@knowledgeplus.net.au>
2018-09-24 21:29:03 +0800

    system maintenance > user > modify access structure

--- includes/vic/custom_processes_user.php
+++ includes/vic/custom_processes_user.php
@@ -674,24 +674,4 @@ $current_signed_in_user_info = JFactory::getUser();
 $custom_functions_user = new CustomFunctionsUser();
 $current_signed_in_user_group_key = $custom_functions_user->getUserGroupKey($current_signed_in_user_info->groups);
 /* ===== end custom processes user ===== */
-
-
-/*
-//testing
-$current_signed_in_user_group_key = '10'; //Victoria Admin
-
-$current_signed_in_user_group_key = '29'; //Victoria Account User
-$current_signed_in_user_group_key = '26'; //Victoria Operation Manager
-$current_signed_in_user_group_key = '30'; //Victoria Site Manager
-
-$current_signed_in_user_group_key = '28'; //Victoria Reception User
-$current_signed_in_user_group_key = '27'; //Victoria Sales Manager
-$current_signed_in_user_group_key = '9'; //Victoria Users
-*/
-// print_r($current_signed_in_user_group_key);
-// exit;
-$csiugk_list = array('10', '29', '26', '30', '28', '27', '9');
-if (isset($_GET['csiugk']) && in_array($_GET['csiugk'], $csiugk_list)) {
-    $current_signed_in_user_group_key = $_GET['csiugk'];
-}
 ?>
