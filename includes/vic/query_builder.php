fb3f049ef8d07d3168d2a96542f05db684ba9692
Joy.Alcorcon <Joy.Alcorcon@knowledgeplus.net.au>
2019-11-11 00:11:14 +0800

    vergola(s)/bsc > builder/tender > existing builder info auto complete

---
+++ includes/vic/query_builder.php
@@ -0,0 +1,58 @@
+<?php
+ ini_set('display_errors', 0);
+
+ $system_config_file_path = $_SERVER['DOCUMENT_ROOT'] . '/configuration.php';
+ $config = array();
+ if (file_exists($system_config_file_path)) {
+     include($system_config_file_path);
+     $system_config = new JConfig();
+     $config['db']['host_name'] = $system_config->host;
+     $config['db']['db_name'] = $system_config->db;
+     $config['db']['user_name'] = $system_config->user;
+     $config['db']['password'] = $system_config->password;
+     $config['path']['log_folder'] = $system_config->log_path . '\\';
+
+     $config['db']['table_name']['builders'] = "ver_chronoforms_data_builderpersonal_lookup";
+ }
+
+ $matches = array(
+     array(
+         'id' => 'Error', 
+         'builder_name' => 'Error'         
+     )
+ );
+ if (count($config) > 0) {
+     $con = mysql_connect(
+         $config['db']['host_name'], 
+         $config['db']['user_name'], 
+         $config['db']['password']
+     );
+
+if ($con) {
+  mysql_select_db($config['db']['db_name']);
+  $state_table = $config['db']['table_name']['builders'];
+
+  $dbname = $config['db']['table_name']['builders'];
+  mysql_select_db($dbname);
+   
+
+  $data = array();
+  $initialSuburbsArray = array( );
+  $term = trim(strip_tags($_GET['term']));
+  $result = mysql_query("SELECT * FROM $state_table where  builder_name LIKE '%{$term}%' ",$con) or die (mysql_error());
+  while( $row = mysql_fetch_assoc( $result ) ) {
+
+    $row['value'] = $row['builder_name'];
+    $row['label'] = $row['builder_name'];
+    
+    array_push($data, $row);
+  }
+}
+  //error_log(print_r($data,true), 3,'C:\\xampp\htdocs\\vergola_contract_system_v4_sa\\my-error.log');
+  $data = array_slice($data, 0, 10);
+  
+  echo json_encode ($data); 
+  mysql_close($con);
+}
+
+?>
