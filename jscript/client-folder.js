442b4ed2d8cb86cb538b43ef3283162bdb53f0ff
Joy Alcorcon <joy.alcorcon@knowledgeplus.net.au>
2021-10-12 14:09:47 +0800

    task: vergola (s) > client / builder > accounts user cannot edit client / builder details

--- jscript/client-folder.js
+++ jscript/client-folder.js
@@ -8,7 +8,8 @@ $(document).ready(function(){
 	if(user_group=="account_user"){
 		is_account_user=1;
 	 
-		$(".btn-delete, .btn-save, .btn-add, .modification-button-holder, .remove-link, .edit-link").each(function(){
+		// $(".btn-delete, .btn-save, .btn-add, .modification-button-holder, .remove-link, .edit-link").each(function(){
+		$(".btn-delete, .btn-add, .modification-button-holder, .remove-link").each(function(){
 			
 			$(this).hide();
 
