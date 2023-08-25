2a078fa22719e40075cf2413068bf5434a267b35
Joy.Alcorcon <Joy.Alcorcon@knowledgeplus.net.au>
2023-08-25 09:48:20 +0800

    vergola (s) > la > system management > lead listing > modify listing table

--- includes/vic/system/lead_lists_vic.php
+++ includes/vic/system/lead_lists_vic.php
@@ -1,4 +1,14 @@
-<?php
+<?php 
+$marketing_source = "";
+$notes = "";
+$lead = "";
+
+if(isset($_POST['marketing_source']))
+{
+	$marketing_source = $_POST['marketing_source'];
+	$sql = "SELECT marketing_source FROM  ver_chronoforms_data_lead_vic AS l ";
+}
+//our pagination function is now in this file
 function pagination($current_page_number, $total_records_found, $query_string = null)
 {
 	$page = 1;
@@ -10,50 +20,70 @@ function pagination($current_page_number, $total_records_found, $query_string = 
 		if ($page != $current_page_number)
 			echo "<a href=\"" . "system-management-vic/lead-listing-vic" . "?page=$page" . (($query_string) ? "&$query_string" : "") . "\">";
 
-		 if ($page == $current_page_number) {echo "<span class=\"current\">$page</span>";} else {echo "$page";}
+		if ($page == $current_page_number) {echo "<span class=\"current\">$page</span>";} else {echo "$page";}
 
-		
 		if ($page != $current_page_number)
 			echo "</a>";
 
 		$page++;
 	}
 }
-
-
+ 
 
 define("NUMBER_PER_PAGE", 100); //number of records per page of the search results
 $instance =& JURI::getInstance();
 $url = JURI::getInstance()->toString();
 
+echo "<div class='search-listing'>
+
+<form id='filter_form'  method='post' method='post' style='float:none; width:90%;'>
+	<label>Marketing Source:</label> 
+	<select name='marketing_source' id='select_marketing_source' style='font-size:14px; padding:4px; min-width:100px;' onchange='document.getElementById(\"filter_form\").submit();'>
+		<option value=\"\"  >Select All</option>
+		<option value='Print' ".($marketing_source=="Print" ? "selected":"") .">Print</option>  
+		<option value='Digital' ".($marketing_source=="Digital" ? "selected":"") .">Digital</option>
+		<option value='Misc.' ".($marketing_source=="Misc." ? "selected":"") .">Misc.</option>  
+	</select> 
+	<input type='submit' name='filter_item' id='filter_item' value='Filter' class='search-btn' onclick='document.getElementById(\"filter_form\").submit();'  />
+</form>
+
+</div>";
+
 //display the search form
 echo "<div class='search-listing'>
-<form action='" . JRoute::_($url) . "' method='post'>
-	Search: <input type='text' name='search_string' /> <input type='submit' name='submit' value='Search' class='search-btn' />
+<form action='" . JRoute::_($url) . "' method='post' style='float:none; width:90%;'>
+	<label>Search:</label> <input type='text' name='search_string' /> <input type='submit' name='submit' value='Search' class='search-btn' style='width:217px;' />
+	<input type='button' class='add-btn' onclick=location.href='" . JURI::base() . "system-management-vic/lead-listing-vic/lead-vic' value='Add New'>
 </form>
-<input type='button' class='add-btn' onclick=location.href='" . JURI::base() . "system-management-vic/lead-listing-vic/lead-vic' value='Add New'>
+
 </div>";
 
 //load the current paginated page number
-$page = ($_GET['page']) ? $_GET['page'] : 1;
+$page = (isset($_GET['page'])) ? $_GET['page'] : 1;
 $start = ($page-1) * NUMBER_PER_PAGE;
 
 /**
 * if we used the search form use those variables, otherwise look for
 * variables passed in the URL because someone clicked on a page number
 **/
-$search = $_POST['search_string'];
-$sql = "SELECT * FROM ver_chronoforms_data_lead_vic WHERE 1=1";
+$search = (isset($_POST['search_string'])?$_POST['search_string']:"");
+$sql = "SELECT marketing_source, lead, notes, cf_id FROM ver_chronoforms_data_lead_vic WHERE 1=1 ";
 $result = mysql_query($sql) or die(mysql_error());
 
-if ($search)
-	$sql .= " AND lead LIKE '%"  . $search .  "%'";
-	
+if ($search){
+	$sql .= " AND marketing_source LIKE '%"  . $search .  "%'" . " OR lead LIKE '%"  . $search .  "%'" . " OR notes LIKE '%"  . $search .  "%'" ;
+}
+//$sql .= " AND marketing_source='{$marketing_source}' ";
+if(strlen($marketing_source)>0){
+	$sql .= " AND marketing_source='{$marketing_source}' "; 
+}
+
+
 //this return the total number of records returned by our query
 $total_records = mysql_num_rows(mysql_query($sql));
 
 //now we limit our query to the number of results we want per page
-$sql .= " LIMIT $start, " . NUMBER_PER_PAGE;
+$sql .= " ORDER BY marketing_source ASC, lead ASC LIMIT $start, " . NUMBER_PER_PAGE;
 
 /**
 * Next we display our pagination at the top of our search results
@@ -61,18 +91,24 @@ $sql .= " LIMIT $start, " . NUMBER_PER_PAGE;
 * this information to the page numbers. That way as they click from page
 * to page the query will pull up the correct results
 **/
-echo "<center><h1 class='search-records'>" . number_format($total_records) . " Records Found</h1></center>";
+//echo "<center><h1 class='search-records'>" . number_format($total_records) . " Records Found</h1></center>";
 echo "<div class='pagination-layer'>";
-pagination($page, $total_records, "lead=$lead");
+pagination($page, $total_records, "marketing_source=$marketing_source&lead=$lead&notes=$notes");
 echo "</div>";
 
 $loop = mysql_query($sql)
 	or die ('cannot run the query because: ' . mysql_error());
-	echo "<table class='listing-table table-bordered'><thead><tr><th>Contract Source</th> </tr></thead><tbody>";
-while ($record = mysql_fetch_assoc($loop))
-    echo "<tr class='pointer' onclick=location.href='" . $this->baseurl . "lead-listing-vic/lead-updatelist-vic?cf_id={$record['cf_id']}' ><td>{$record['lead']}</td></tr>";
+	// echo "<table class='listing-table table-bordered'><thead><th style='width: 20%;'>Marketing Source</th><th>Lead Source</th><th>Notes</th></thead><tbody>";
+	echo "<table class='listing-table table-bordered'><thead><th style='width: 20%;'>Marketing Source</th><th>Lead Source</th></thead><tbody>";
+while ($record = mysql_fetch_assoc($loop)){
+    echo "<tr class='pointer' onclick=location.href='" . $this->baseurl . "lead-listing-vic/lead-updatelist-vic?cf_id={$record['cf_id']}' >";
+		// echo "<td>{$record['marketing_source']}</td> " . "<td>{$record['lead']}</td>" . "<td>{$record['notes']} </td>";
+    echo "<td>{$record['marketing_source']}</td> " . "<td>{$record['lead']}</td>";
+	echo "</tr>";
+	echo "<tr></tr>";
+}		
     echo "</tbody></table>"; 
     
 echo "<div class='pagination-layer'>";
-pagination($page, $total_records, "lead=$lead");
+pagination($page, $total_records, "marketing_source=$marketing_source&lead=$lead&notes=$notes");
 echo "</div>";
--- includes/vic/system/lead_vic.php
+++ includes/vic/system/lead_vic.php
@@ -1,36 +1,56 @@
 <?php  
 if(isset($_POST['add']))
-{	
-	$Lead_add=$_POST['lead'] ;
-				
+{ 
+  $Marketing_Source_add=$_POST['marketing_source'] ;
+  $Lead_add=$_POST['lead'] ;
+  // $Marketing_Source_add = "Test";
+        
 
 
-	mysql_query("INSERT INTO ver_chronoforms_data_lead_vic (lead) 
-		 VALUES ('$Lead_add')");
-			
-	
-	
-	header('Location:'.JURI::base().'system-management-vic/lead-listing-vic');		
+  mysql_query("INSERT INTO ver_chronoforms_data_lead_vic (marketing_source,lead) 
+     VALUES ('$Marketing_Source_add','$Lead_add')");
+      
+  
+  
+  header('Location:'.JURI::base().'system-management-vic/lead-listing-vic');    
 }
 
 //if(isset($_POST['cancel']))
-//{	
-//	header('Location:'.JURI::base().'system-management-vic/lead-listing-vic');	
+//{ 
+//  header('Location:'.JURI::base().'system-management-vic/lead-listing-vic');  
 //}
 
+$cbo_marketing_source = "<select name=\"marketing_source\" style='width:100%; padding:0px'><option value=''>Select Marketing Source</option>"; 
+$querysub="SELECT * FROM ver_chronoforms_data_lead_vic WHERE marketing_source != '' GROUP BY marketing_source ORDER BY marketing_source ASC";
+      $resultsub = mysql_query($querysub);
+        if(!$resultsub){die ("Could not query the database: <br />" . mysql_error()); }            
+      while ($data=mysql_fetch_assoc($resultsub)){  
+        // $cbo_marketing_source .= "<option value = \"".addslashes($data['marketing_source'])."\">{$data['marketing_source']}</option>";
+        $cbo_marketing_source .= "<option value = \"".addslashes($data['marketing_source'])."\" selected>{$data['marketing_source']}</option>";
+        // if($data['marketing_source']==$contract_vergola['marketing_source']){ 
+        //   $cbo_marketing_source .= "<option value = \"".addslashes($data['marketing_source'])."\" selected>{$data['marketing_source']}</option>";
+        // }else{
+        //   $cbo_marketing_source .= "<option value = \"".addslashes($data['marketing_source'])."\">{$data['marketing_source']}</option>";
+        // } 
+      }
+$cbo_marketing_source .= "</select>"; 
 ?>
 
 <form class="Chronoform hasValidation" method="post" id="chronoform_Lead_Vic" action="<?php echo JURI::base(); ?>system-management-vic/lead-listing-vic/lead-vic">
-<table class="update-table">
+<table class="update-table" style="width: 50%;">
    
-	<tr>
-		<td class="row1">Lead Source</td>
-		<td class="row2"><input type="text" name="lead" value="<?php echo $Lead_add ?>"/></td>
-	</tr>
-	<tr>
-		<td class="row1">&nbsp;</td>
-		<td class="row2"><input type="submit" name="add" value="Save" class="update-btn" /> <input type="button" name="cancel" value="Cancel" class="update-btn" onclick=location.href='<?php echo JURI::base(); ?>system-management-vic/lead-listing-vic' /></td>
-	</tr>
+  <tr>
+    <td class="row1" >Marketing Source :</td>
+    <td class="" style="width: 20%;"><?php echo $cbo_marketing_source; ?></td>
+    <td class="row1" style="width: 12%;">Lead Source :</td>
+    <td class="" ><input style="width: 100%;" type="text" name="lead" value="<?php echo $Lead_add ?>"/></td>
+  </tr>
+  <tr>
+    <td class="row1">&nbsp;</td>
+    <td class="">&nbsp;</td>
+    <td class="">&nbsp;</td>
+    <td class="row2"><input type="submit" name="add" value="Save" class="update-btn" /> <input type="button" name="cancel" value="Cancel" class="update-btn" onclick=location.href='<?php echo JURI::base(); ?>system-management-vic/lead-listing-vic' /></td>
+  </tr>
 </table>
 
 </form>
